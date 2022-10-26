51ec41859911a4879e2d9657a3ae147a15c3cc9c
jezeenaaa <724214@student.dusd.net>
2022-10-26 15:17:30 -0700

    Added lines of code to script
    I added more lines of code to Ruby's script. Now she moves horizontally and vertically using the arrow keys.

--- RubysAdventureRushP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureRushP7/Assets/Scripts/RubyController.cs
@@ -14,9 +14,12 @@ public class RubyController : MonoBehaviour
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
+        float vertical = Input.GetAxis("Vertical");
+
+        
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
