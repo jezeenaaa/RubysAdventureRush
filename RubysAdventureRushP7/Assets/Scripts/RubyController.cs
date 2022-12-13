0efe8604715a3f570355825f98902ca262f48740
jezeenaaa <116121135+jezeenaaa@users.noreply.github.com>
2022-12-13 13:17:09 -0800

    Fixed Ruby Script and Finished Ui Health bar script
    i fixed the ruby script for the ui healthbar and i also finished writing the ui health bar script

--- RubysAdventureRushP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureRushP7/Assets/Scripts/RubyController.cs
@@ -1,48 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
-         
+
     public int maxHealth = 5;
+
+    public GameObject projectilePrefab;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
     public float timeInvincible = 2.0f;
-
-    public int health { get { return currentHealth; }}
-    public int currentHealth;
-
     bool isInvincible;
     float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
-    public GameObject projectilePrefab;
 
     Animator animator;
-    Vector2 lookDirection = new Vector2(1,0);
+    Vector2 lookDirection = new Vector2(1, 0);
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
         currentHealth = maxHealth;
-
-        animator = GetComponent<Animator>();
-       
     }
 
     // Update is called once per frame
     void Update()
-    {   // create variables to use the built in axis
+    {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
         Vector2 move = new Vector2(horizontal, vertical);
 
-        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
         {
             lookDirection.Set(move.x, move.y);
             lookDirection.Normalize();
@@ -52,7 +50,6 @@ public class RubyController : MonoBehaviour
         animator.SetFloat("Look Y", lookDirection.y);
         animator.SetFloat("Speed", move.magnitude);
 
-
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
@@ -60,56 +57,44 @@ public class RubyController : MonoBehaviour
                 isInvincible = false;
         }
 
-        if(Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.C))
         {
             Launch();
         }
-
-       
-       
-
     }
 
     void FixedUpdate()
     {
-        // create a movement vector
         Vector2 position = rigidbody2d.position;
-        //make horizontal and vertical movement work
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
-
-
     }
+
     public void ChangeHealth(int amount)
     {
         if (amount < 0)
         {
-            animator.SetTrigger("Hit");
             if (isInvincible)
                 return;
 
             isInvincible = true;
             invincibleTimer = timeInvincible;
-
-
-
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
 
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
-    
+
     void Launch()
     {
         GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
         Projectile projectile = projectileObject.GetComponent<Projectile>();
         projectile.Launch(lookDirection, 300);
 
         animator.SetTrigger("Launch");
     }
-
-    
 }
--- RubysAdventureRushP7/Assets/Scripts/UIHealthBar.cs
+++ RubysAdventureRushP7/Assets/Scripts/UIHealthBar.cs
@@ -10,20 +10,18 @@ public class UIHealthBar : MonoBehaviour
     public Image mask;
     float originalSize;
 
-    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+    }
+
     void Start()
     {
         originalSize = mask.rectTransform.rect.width;
     }
 
-    // Update is called once per frame
     public void SetValue(float value)
     {
         mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize * value);
-    }
-
-    void Awake()
-    {
-        instance = this;
     }
 }
